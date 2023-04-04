b059f5e90a2529fd42529af372a27d6cacb893d5
Christian Magana <cmagana2003@gmail.com>
2023-04-04 12:21:42 -0700

    PsExec time I guess

--- test.go
+++ test.go
@@ -1,42 +1,50 @@
 package main
 
 import (
-	"fmt"
 	"io/ioutil"
-	"myproject/assets"
+	"log"
 	"os"
 	"os/exec"
+
+	"github.com/M00NLIG7/GoAway/assets"
 )
 
 
 func main() {
 	// Retrieve the contents of Psexec.exe from the embedded assets
-	data, err := assets.Must
+	data, err := assets.Asset("psexec.exe")
 	if err != nil {
 		panic(err)
 	}
 
-	// Write the contents of Psexec.exe to a temporary file
-	tmpfile, err := ioutil.TempFile("", "psexec.*.exe")
+// Create a temporary file to write the binary data to
+	f, err := ioutil.TempFile("", "psexec")
 	if err != nil {
-		panic(err)
+		log.Fatalf("failed to create temporary file: %s", err)
 	}
-	defer os.Remove(tmpfile.Name()) // clean up the temporary file when we're done
-	_, err = tmpfile.Write(data)
+	defer os.Remove(f.Name())
+
+	// Write the binary data to the temporary file
+	_, err = f.Write(data)
 	if err != nil {
-		panic(err)
-	}
-	err = tmpfile.Close()
-	if err != nil {
-		panic(err)
+		log.Fatalf("failed to write binary data to temporary file: %s", err)
 	}
 
-	// Execute the temporary file with the specified command-line arguments
-	cmd := exec.Command(tmpfile.Name(), "\\\\REMOTE_HOSTNAME", "-u", "USERNAME", "-p", "PASSWORD", "COMMAND")
-	output, err := cmd.CombinedOutput()
+	// Make the temporary file executable
+	err = os.Chmod(f.Name(), 0700)
 	if err != nil {
-		panic(err)
+		log.Fatalf("failed to make temporary file executable: %s", err)
 	}
 
-	fmt.Println(string(output))
+	// PSEXEC.EXE \\LocalComputerIPAddress -u DOMAIN\my-user -p mypass CMD
+
+	// Execute psexec.exe from the temporary file
+	cmd := exec.Command(f.Name(), "\\192.168.1.21", "-u", "Administrator", "-p", "Password123", "CMD")
+	out, err := cmd.Output()
+	if err != nil {
+		log.Fatalf("failed to execute psexec.exe: %s", err)
+	}
+
+	// Print the output of the command
+	log.Printf("psexec.exe output: %s", string(out))
 }
